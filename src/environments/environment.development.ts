14eb5adf93623cd8977763e96d9f50ea51ba5695
Alexander Mamani Yucra <x23329823@student.ncirl.ie>
2024-11-11 21:00:17 +0000

    shore: configure development and production environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  url: "18.212.161.71",
+  production: false,
+  url: "http://localhost:8000",
   wsUrl: "wss://9kii4tuqxi.execute-api.us-east-1.amazonaws.com/staging/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  url: "http://localhost:8000",
-  wsUrl: "wss://ywz0p0fvr4.execute-api.us-east-1.amazonaws.com/staging/"
+  production: true,
+  url: "18.212.161.71",
+  wsUrl: "wss://9kii4tuqxi.execute-api.us-east-1.amazonaws.com/staging/"
 };
