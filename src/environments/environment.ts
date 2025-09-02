9087fe4d77ff93a24d11a1f465cd5ad3fa4ddfee
Alexander Mamani Yucra <akeymy4@gmail.com>
2025-09-02 14:47:58 +0100

    https references was changed in config file due the changing ssl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: true,
-  url: "http://alexandermamani.com",
+  url: "https://alexandermamani.com",
   wsUrl: "wss://hjmzgi77i3.execute-api.us-east-1.amazonaws.com/staging/",
   qr_code_service_host: "http://54.154.139.81:5000/"
 };
